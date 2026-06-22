6cdb39f17ab338716b61193e7c5ff6ab5b085328
Pontear <emanuelbolivar099@gmail.com>
2026-06-22 09:43:41 -0500

    Agregado formulario de clientes y actualización de conexión

--- Conexion/ConexionDB.vb
+++ Conexion/ConexionDB.vb
@@ -6,7 +6,6 @@ Namespace Conexion
         Private Shared cadenaconexion As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & rutaBaseDatos
 
         Public Shared Function AbrirConexion() As OleDbConnection
-            ' Simplemente crea el objeto, no lo abras aquí
             Return New OleDbConnection(cadenaconexion)
         End Function
     End Class
